da13f2a60f099ce3c76e0bce45050c04edcb49c4
Avishak Deb Roy <95048039+avishakdebroy@users.noreply.github.com>
2022-09-20 15:24:06 -0400

    update

--- src/main/java/com/perscholas/MyController.java
+++ src/main/java/com/perscholas/MyController.java
@@ -1,16 +1,30 @@
 package com.perscholas;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("home")
-
 public class MyController {
+
     @RequestMapping("/login")
-    public String login() {
-        return "";
+    public String showLogin() {
+        return "inboxpage";
     }
 
+    @RequestMapping("/")
+    public String simpleMethod() {
+        return "inboxpage";
+    }
+
+    @RequestMapping("/index")
+    public String showIndex() {
+        return "inboxpage";
+    }
+    @GetMapping("/javaindex")
+    public String showJavaMethod() {
+        return "javaindex";
+    }
 
 }
--- src/main/java/com/perscholas/one_rest/OneRestApplication.java
+++ src/main/java/com/perscholas/one_rest/OneRestApplication.java
@@ -3,7 +3,7 @@ package com.perscholas.one_rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.perscholas")
 public class OneRestApplication {
 
     public static void main(String[] args) {
