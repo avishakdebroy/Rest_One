30343d27ce8c4107c848d180dbb75742b0f53614
Avishak Deb Roy <95048039+avishakdebroy@users.noreply.github.com>
2022-09-20 14:37:56 -0400

    initial commit

---
+++ src/main/java/com/perscholas/one_rest/OneRestApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.one_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perscholas/one_rest/OneRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.one_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
